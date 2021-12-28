24aef1f950b117a893ffedb402557dcb6a175965
kenzysc <kenzysc@gmail.com>
2021-12-28 10:14:44 +0100

    added build file from u-exchange

---
+++ static/js/7.ebaa8d36.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[7],{1013:function(n,i){},920:function(n,i){},921:function(n,i){},930:function(n,i){},931:function(n,i){},951:function(n,i){},954:function(n,i){},955:function(n,i){},964:function(n,i){}}]);
+//# sourceMappingURL=7.ebaa8d36.chunk.js.map
